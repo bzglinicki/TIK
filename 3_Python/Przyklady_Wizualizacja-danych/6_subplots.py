c444f34d875f17acd38fed6aa8249db153f0ac6f
Bartłomiej Zglinicki <78939671+bzglinicki@users.noreply.github.com>
2023-11-14 10:26:19 +0100

    Wizualizacja danych - przykłady 2.

--- 3_Python/Przyklady_Wizualizacja-danych/6_subplots.py
+++ 3_Python/Przyklady_Wizualizacja-danych/6_subplots.py
@@ -10,21 +10,48 @@ x = np.linspace(0, 2*np.pi, 200)
 y = np.sin(x**2)
 
 
-fig1, ax = plt.subplots()
-ax.plot(x, y)
-ax.set_title("Pojedynczy wykres.")
+fig, axs = plt.subplots(2, 1)
+fig.suptitle("Wykresy rozmieszczone wertykalnie")
+axs[0].plot(x, y)
+axs[1].plot(x, -y)
+plt.show()
+
+fig, axs = plt.subplots(2, 1, sharex = True)
+fig.suptitle("Wykresy rozmieszczone wertykalnie - wspólna oś x.")
+axs[0].plot(x, y)
+axs[1].plot(x + 1, -y)
+plt.show()
+
+fig, axs = plt.subplots(3, 1, sharex = True, sharey = True)
+fig.suptitle("Wykresy rozmieszczone wertykalnie - wspólna obie osie.")
+axs[0].plot(x, y ** 2)
+axs[1].plot(x, 0.3 * y, 'o')
+axs[2].plot(x, y, '+')
 plt.show()
 
 
-fig2, axs = plt.subplots(2)
-fig2.suptitle("Wykresy rozmieszczone wertykalnie")
+fig, axs = plt.subplots(1, 2)
+fig.suptitle("Wykresy rozmieszczone horyzontalnie")
 axs[0].plot(x, y)
 axs[1].plot(x, -y)
 plt.show()
 
 
-fig3, (ax1, ax2) = plt.subplots(1, 2)
-fig3.suptitle("Wykresy rozmieszczone horyzontalnie")
-ax1.plot(x, y)
-ax2.plot(x, -y)
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].plot(x, y)
+axs[0, 0].set_title('Axis [0, 0]')
+axs[0, 1].plot(x, y, 'tab:orange')
+axs[0, 1].set_title('Axis [0, 1]')
+axs[1, 0].plot(x, -y, 'tab:green')
+axs[1, 0].set_title('Axis [1, 0]')
+axs[1, 1].plot(x, -y, 'tab:red')
+axs[1, 1].set_title('Axis [1, 1]')
+
+for ax in axs.flat:
+    ax.set(xlabel='x-label', ylabel='y-label')
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for ax in axs.flat:
+    ax.label_outer()
+
 plt.show()
